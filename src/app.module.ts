26af8e3f72427bf27e0b23bdc774abee83449e16
altristan <al.tristan.bandiola@eee.upd.edu.ph>
2020-10-13 00:42:14 +0800

    added blog CRUD api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import {ConfigModule} from "@nestjs/config";
 import {MongooseModule} from "@nestjs/mongoose";
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import {MongooseModule} from "@nestjs/mongoose";
       useUnifiedTopology: true,
       useCreateIndex: true,
     }),
-      AuthModule],
+      AuthModule,
+      BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
